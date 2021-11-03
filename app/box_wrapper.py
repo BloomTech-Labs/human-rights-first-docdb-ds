775ae227e9ec8629264f5a0c78caaa9a23b36e3e
Daos21912 <DanFerber912@gmail.com>
2021-11-03 12:46:53 -0400

    changed output from box_wrapper methods to dictionary or list of dictionary or bytes.
    Co-authored-by: Peter Rockwood <peter.rockwood@gmail.com>
    Co-authored-by: Jackson Reber <jacksonreber@yahoo.com>
    Co-authored-by: Hunter Jordan <hunterg96@gmail.com>
    Co-authored-by: Youssef Al-Yakoob <yalyakoob@gmail.com>
    >

--- app/box_wrapper.py
+++ app/box_wrapper.py
@@ -19,28 +19,59 @@ class BoxWrapper:
     client = Client(auth)
 
     def items_in_folder(self, folder_id=base_folder_id):
-        return self.client.folder(folder_id=folder_id).get_items()
+        items = self.client.folder(folder_id=folder_id).get_items()
+        files = []
+        folders = []
+        for item in items:
+            dtc = {
+                "id": item.id,
+                "name": item.name,
+                "type": item.type
+            }
+            if item.type == "file":
+                files.append(dtc)
+            else:
+                folders.append(dtc)
+        return files, folders
 
     def get_file_info(self, file_id):
         file_info = self.client.file(file_id).get()
-        return file_info
+        dtc = {
+            "id": file_id,
+            "name": file_info.name,
+            "ext": file_info.name[-3:],
+            "path": "/".join(s.name for s in file_info.path_collection['entries']) + f"/{file_info.name}",
+            "parent_folder": {
+                "id": file_info.path_collection['entries'][-1].id,
+                "name": file_info.path_collection['entries'][-1].name,
+                "url": "https://app.box.com/folder/" + file_info.path_collection['entries'][-1].id,
+            },
+            "url": "https://app.box.com/file/" + file_id
+        }
+        return dtc
 
     def get_thumbnail(self, file_id):
         return self.client.file(file_id).get_thumbnail_representation('92x92', extension='jpg')
 
-    def download_file(self, file_id: str) -> Union[Tuple[str, bytes], None]:
+    def download_file(self, file_id: str) -> [bytes, None]:
         info = self.get_file_info(file_id)
-        pth = "/".join(s.name for s in info.path_collection['entries']) + f"/{info.name}"
-        if info.name[-3:] == 'pdf':
+        if info['ext'] == 'pdf':
             cont = self.client.file(file_id).content()
-            return pth, cont
+            return cont
 
 
 if __name__ == '__main__':
     box = BoxWrapper()
-    items_in_fold = list(box.items_in_folder())
-    fold = items_in_fold[0]
-    items_in_fold = list(box.items_in_folder(fold.id))
-    file_path, bts = box.download_file(items_in_fold[0].id)
-    print(file_path)
-    print(len(bts))
+    inf = box.get_file_info("873662819506")
+    for key, val in inf.items():
+        print(f"{key} : {val}")
+    # items_fold = box.items_in_folder("129133191949")
+    # print(items_fold)
+    # for item in items_fold:
+    #     print(f"ID: {item.id}, name: {item.name}, type: {item.type}")
+    # items_in_fold = list(box.items_in_folder())
+    # fold = items_in_fold[0]
+    # items_in_fold = list(box.items_in_folder(fold.id))
+    # file_path, bts = box.download_file(23470514533)
+    # print(file_path)
+    # print(len(bts))
